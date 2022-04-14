52cd967a839633afa80309035e754e8a7f916978
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2022-04-14 11:02:32 -0500

    firstCommit

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const Color primary = Color(0x090807);
+const Color secondary = Color(0xC4C4C4);
+const Color cardBackground = Color(0x3D3D3D);
+const Color success = Color(0x19AD8A);
+const Color danger = Color(0xB45555);
+const Color orangeDark = Color(0xA9500F);
+const Color textWhite = Color(0xFFFFFF);
+const Color textOrange = Color(0xF69B59);
---
+++ lib/src/theme/fontsizes.dart
@@ -0,0 +1,5 @@
+const double titleFontSize = 34;
+const double subTitleFontSize = 20;
+const double subTitleTwoFontSize = 18;
+const double hintTextFontSize = 13;
+const double smallTextFontSize = 12;
